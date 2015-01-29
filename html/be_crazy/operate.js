eae74077f13900faa359d9d3c64b3831c0040b61
Inho Won <wonderino@gmail.com>
2015-01-29 19:23:44 +0900

    add a file

---
+++ html/be_crazy/operate.js
@@ -0,0 +1,9 @@
+(function() {
+  var path = 'data.csv'
+  d3.csv(path, function(data) {
+      var chart = d3.beCrazy().source('출처: 알라딘북스');
+      d3.select('div.chart.crazy_books')
+        .datum(data)
+        .call(chart);
+  });
+}());
